9f4070e39e59cebc74ab3c69ee4a1728ff8c54cb
Ishibashi <ishibashi.futos@jp.fujitsu.com>
2019-10-02 14:28:48 +0900

    クラス名を変更、対象例外を絞る

--- src/main/java/com/example/samplewebapp/fw/ValidationExceptionHandler.java
+++ src/main/java/com/example/samplewebapp/fw/ValidationExceptionHandler.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import org.springframework.validation.BindException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.context.request.WebRequest;
@@ -13,15 +15,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class BindExceptionHandler {
+public class ValidationExceptionHandler {
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler({
-    org.springframework.web.bind.MethodArgumentNotValidException.class,
-    IllegalArgumentException.class,
-    BindException.class,
-    com.fasterxml.jackson.databind.exc.InvalidDefinitionException.class,
-    org.springframework.http.converter.HttpMessageConversionException.class
+    ConstraintViolationException.class, // パスパラメータまたは引数で指定するケース
+    InvalidDefinitionException.class, // JacksonがEnumをBindするときに失敗すると出る
+    BindException.class, // 引数でDTOを指定するケース
+    IllegalArgumentException.class, // 独自バリデータが投げる例外
+    BindException.class // 引数で@RequestBodyを指定するケース
   })
   public Map<String, Object> handleBindingException(Exception e, WebRequest request) {
       Map<String, Object> errorMap = new HashMap<String, Object>();
